eebaca3edc3d6a187ac7b1558fb5b1b236b0a567
Junaid Baig <junaidb790@gmail.com>
2019-05-17 17:51:51 -0400

    Cleaned up the clutter

--- main.py
+++ main.py
@@ -1,21 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import multiprocessing as mp
-import time
+import math
+import tkinter
+
+from tkinter import *
+fields = 'xo', 'xf', 'yo', 'yf', 'Function'
+
+def function(x,y,func):
+    return eval(func)
+
+def getVal(entries, text):
+    a = float(entries[text].get())
+    return a
+
+def getFunc(entries):
+    f = str(entries['Function'].get())
+    return f
+
+def hur(entries):
+    global xo
+    global xf
+    global yo
+    global yf
+    global f
+    xo = getVal(entries, 'xo') 
+    xf = getVal(entries, 'xf') 
+    yo = getVal(entries, 'yo') 
+    yf = getVal(entries, 'yf') 
+    f = getFunc(entries)
+    root.quit()
+
+def makeform(root, fields):
+   entries = {}
+   for field in fields:
+      row = Frame(root)
+      lab = Label(row, width=22, text=field+": ", anchor='w')
+      ent = Entry(row)
+      ent.insert(0,"0")
+      row.pack(side=TOP, fill=X, padx=5, pady=5)
+      lab.pack(side=LEFT)
+      ent.pack(side=RIGHT, expand=YES, fill=X)
+      entries[field] = ent
+   return entries
+
+if __name__ == '__main__':
+   root = Tk()
+   root.title("Equation Grapher")
+   ents = makeform(root, fields)
+   b1 = Button(root, text='Calculate',
+          command=(lambda e=ents: hur(e)))
+   b1.pack(side=LEFT, padx=5, pady=5)
+   root.mainloop()
+
+   print (xo)
+   print (xf)
+   print (yo)
+   print (yf)
+   print (f)
 
 
-def function(x,y):
-    return x**2 + y**2
+#getting "dx" and "dy"
+dx = float(xf - xo) / 100
+dy = float(yf - yo) / 100
 
-start = time.time()
+x = np.arange(xo, xf, dx)
+y = np.arange(yo, yf, dy)
 
 plt.figure()
-
-#pool = mp.Pool(mp.cpu_count())
-
-x = np.arange(0, 1, 0.01)
-y = np.arange(0, 1, 0.01)
-
 a = len(x)
 b = len(y)
 
@@ -23,17 +74,9 @@ X,Y = np.meshgrid(x, y)
 
 Zmatrix = [[0 for c in range(a)] for d in range(b)]
 
-for i in range(0,99):
-    for j in range(0, 99):
-        #Zmatrix[i][j] = (x[j] + j)**2 + (y[i]+i)**2
-        Zmatrix[i][j] = function(x[j] + j, y[i] + i)
+for i in range(0, int(math.floor(xf/dx) - 1)):
+    for j in range(0, int(math.floor(yf/dy) - 1)):
+        Zmatrix[i][j] = function(x[j] + j, y[i] + i, f)
 
 plt.contour(X, Y, Zmatrix, 100)
-plt.grid(True)
-#plt.show()
-
-end = time.time()
-
-print(end-start)
-
 plt.show()
